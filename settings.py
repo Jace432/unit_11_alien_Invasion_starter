fbbbaef9c4ca901e0e2c106da8742ea7cbb7c9e7
Jace432 <jwebb54@student.cscc.edu>
2025-04-04 14:48:17 -0400

    Added events for ship moving left and right, built in boundaries

--- settings.py
+++ settings.py
@@ -9,6 +9,9 @@ class Settings:
         self.bg_file   = Path.cwd() / "Assets" / "images"  / "Starbasesnow.png"
         
         self.ship_file = Path.cwd() / "Assets" / "images" / "ship2(no bg).png"
-        self.ship_w   = 40
-        self.ship_h   = 60
+        self.ship_w     = 40
+        self.ship_h     = 60
+        self.ship_speed = 5
+
+
 
