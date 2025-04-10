16279bfac6c4cb5ea32fce0f0d120dc348f862e0
Jace432 <jwebb54@student.cscc.edu>
2025-04-10 13:45:29 -0400

    P II - set up alien fleet for sound and collisions with projectiles and bottom

--- alien_invasion.py
+++ alien_invasion.py
@@ -52,7 +52,18 @@ class AlienInvasion:
             self._reset_level()
             # subtract one life, if possible
 
-        
+        # check colisions for aliens and bottom of screen 
+        if self.alien_fleet.check_fleet_bottom():
+            self._reset_level()
+
+            
+
+
+        # check colisions of projectiles and aliens
+        collisions = self.alien_fleet.check_collisions(self.ship.arsenal.arsenal)
+        if collisions:
+            self.impact_sound.play()
+            self.impact_sound.fadeout(500)
         pass
 
     def _reset_level(self):
